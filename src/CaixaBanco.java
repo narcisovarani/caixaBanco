b9e7729c6fee2eb67bff959a59df6def0660d951
cleiton071 <cleiton071@hotmail.com>
2022-06-15 17:12:10 -0300

    add usuarios

--- src/CaixaBanco.java
+++ src/CaixaBanco.java
@@ -1,66 +1,68 @@
 import java.util.ArrayList;
 
 public class CaixaBanco {
-    private String usuarioLogado;
+    private Integer usuarioLogado;
     private Boolean logado = false;
-    /*private ArrayList<Object> usuarios = new ArrayList<>();*//**/
+    private ArrayList<Usuario> usuarios = new ArrayList<>();
 
- Usuario user1 = new Usuario(2000.00, "324.360.618-74", "123", "Narciso");
+    public void setUsuarios(){
+        usuarios.add(new Usuario(2000.00, "324.360.618-74", "123", "Narciso"));
+    }
 
-
-    /*public CaixaBanco(ArrayList<Object> usuarios) {
-        this.usuarios = usuarios;
-
-        usuarios.add(new Usuario(2000.00, "324.360.618-74", "123", "Narciso"));*//**//*
-    }*/
-
-    /*public void setUsuarios(){
-    usuarios.add(new Usuario(2000.00, "324.360.618-74", "123", "Narciso"));
-}*/
-
-
-    public Boolean verificaLogin(String cpf, String nomeDeUsuario, String senha) {
+    public Integer verificaLogin(String cpf) {
         if (logado) {
-            if (user1.getCpf() == cpf) {
-                return true;
-            }
-            else {
-                logado=true;
-                user1.setCpf(cpf);
-                user1.setSenha(senha);
-                this.usuarioLogado= user1.getNome();
-                return true;
-            }
+            return -2;
         }
 
-        logado=true;
-        user1.setCpf(cpf);
-        user1.setSenha(senha);
-        this.usuarioLogado= user1.getNome();
+        Integer i = 0, userExist = -1;
+        for (i=0; i<usuarios.size(); i++){
+            if (cpf.equals(usuarios.get(i).getCpf())){
+               userExist = i;
+               break;
+            }
+        }
+        return userExist;
 
-        return true;
     }
-    public void logar(String cpf, String senha, String nomeDeUsuario) {
-        verificaLogin(cpf, nomeDeUsuario, senha);
-        System.out.printf("usuario logado: %s", this.usuarioLogado);
-
+    public void logar(String cpf, String senha) {
+        Integer userExist = verificaLogin(cpf);
+         if (userExist == -1){
+             System.out.println("Usuario digitado nao exite");
+         }
+         else if (userExist == -2) {
+             System.out.println("ja existe usuario logado no sistema. \n por favor");
+         }
+         else {
+             if (senha.equals(usuarios.get(userExist).getSenha())){
+               usuarioLogado = userExist;
+               logado = true;
+               System.out.printf("usuario logado: %s\n", usuarios.get(usuarioLogado).getNome());
+             }
+             else System.out.println("senha incorreta");
+         }
     }
 
     public void depositar(Double valor){
-        user1.setSaldo(valor + user1.getSaldo());
-        System.out.printf("\nSaldo Atual: %.2f ", user1.getSaldo());
+        usuarios.get(usuarioLogado).setSaldo(valor + usuarios.get(usuarioLogado).getSaldo());
+        System.out.printf("\nSaldo Atual: %.2f ", usuarios.get(usuarioLogado).getSaldo());
     }
 
     public void sacar(Double valor){
-        if (user1.getSaldo()-valor<-600.00)
+        if (usuarios.get(usuarioLogado).getSaldo()-valor<-600.00)
         {
             System.out.println("Saque nao realizado, voce ultrapassou seu limite de credito");
 
     }
 
         else {
-            user1.setSaldo(user1.getSaldo() -valor);
-            System.out.printf("saldo atual: %.2f", this.user1.getSaldo());
+            usuarios.get(usuarioLogado).setSaldo(usuarios.get(usuarioLogado).getSaldo() -valor);
+            System.out.printf("saldo atual: %.2f", usuarios.get(usuarioLogado).getSaldo());
         }
+    }
+
+    public void registra(String cpf, String senha, String nome){
+        usuarios.add(new Usuario(00.00, cpf, senha, nome));
+        System.out.printf("usuario %s foi registrado!", nome);
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         CaixaBanco caixa1 = new CaixaBanco();
-        Scanner  scan = new Scanner(System.in);
-/*        *//**//*caixa1.logar("111.222.333-11",  "22", "Narciso ");
-        *//**//*caixa1.depositar(100.00);
-       *//**//*caixa1.sacar(5000.00);*/
-
-
-        System.out.printf("Saldo: %.2f", caixa1.user1.getSaldo());
-
-        caixa1.depositar(100.00);
+        Scanner scan = new Scanner(System.in);
+        caixa1.setUsuarios();
+        caixa1.logar("324.360.618-74", "123");
+        caixa1.registra("123.123.123-00", "000", "novo");
     }
 }
